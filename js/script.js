db0f26e661294137a35112a648dfee8de13aa28c
May <26matmoran@gmail.com>
2024-03-13 19:30:12 -0600

    Add h1 title + simple logic to output random flags

---
+++ js/script.js
@@ -0,0 +1,10 @@
+image = document.querySelector("img");
+let x = "af al dz ad ao ag ar am au at az bs bh bd bb by be bz bj bt bo ba bw br bn bg bf bi kh cm ca cv cf td cl cn co km cg cd cr ci hr cu cy cz dk dj dm do ec eg sv gq er ee et fj fi fr ga gm ge de gh gr gd gt gn gw gy ht hn hu is in id ir iq ie il it jm jp jo kz ke ki kp kr kw kg la lv lb ls lr ly li lt lu mk mg mw my mv ml mt mh mr mu mx fm ma md mc mn me mz mm na nr np nl nz ni ne ng no om pk pw pa pg py pe ph pl pt qa ro ru rw kn lc vc ws sm st sa sn rs sc sl sg sk si sb so za ss es lk sd sr sz se ch sy tj tz th tl tg to tt tn tr tm tv ug ua ae gb us uy uz vu ve vn ye zm zw"
+let flagNames = x.split(" ")
+
+
+images = document.querySelectorAll("img");
+images.forEach(image => {
+    let randomFlag = Math.floor((Math.random() * 193 + 1))
+    image.src = "./resources/images/128x128/" + flagNames[randomFlag] + ".png";
+});
